c94b1df29c2a4e91386c7770e7bb315fe208ad18
ArturoDiaz23 <55565184+ArturoDiaz23@users.noreply.github.com>
2022-11-17 20:30:55 +0000

    modified

--- js/app.js
+++ js/app.js
@@ -1,4 +1,23 @@
+//registro de service worker
 if(navigator.serviceWorker){
     navigator.serviceWorker.register('./sw.js')
     console.log('hola:sw')
 }
+
+//notificaciones
+function msnPush(){
+    Push.Permission.request();
+    Push.create('Pokedex',{
+        body: 'Nuevos pokemon',
+        icon: '/pokedex.png',
+        timeout: 150000,
+        vibrate:[100,100,100],
+        onClick: function(){
+            //window.location='https://arturodiaz23.github.io/pokedex/'
+            window.focus();
+            this.close();
+        }
+    });
+}
+
+msnPush();
